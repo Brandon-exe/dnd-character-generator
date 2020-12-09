6f87f8ec291460d28dcbbbb0633d733354bd4420
Brandon Phillips <brandon.formal@gmail.com>
2020-12-09 10:30:49 -0700

    cont'd work in the get_class data set

--- get_class.py
+++ get_class.py
@@ -14,6 +14,10 @@ def get_class():
                     'simple weapons', 'martial weapons',
                 },
                 'tools': set(),
+                # use random.sample() here to choose two random skills
+                'skills': {
+                    'Animal Handling', 'Athletics', 'Intimidation', 'Nature', 'Perception', 'Survival',
+                },
             },
             'saving throws': {
                 'Str', 'Con',
@@ -35,6 +39,13 @@ def get_class():
                 },
                 'tools': {
                     'three musical instrucments of your choice'
+                },
+                # use random.sample() here to choose 3 random skills
+                'skills': {
+                    'Athletics', 'Acrobatics', 'Sleight of Hand', 'Stealth', 'Arcana', 
+                    'History', 'Investigation', 'Nature', 'Nature', 'Religion', 'Animal Handling'
+                    'Insight', 'Medicine', 'Perception', 'Survival', 'Deception', 'Intimidation',
+                    'Performance', 'Persuasion',
                 },
             },
             'saving throws': {
@@ -59,16 +70,88 @@ def get_class():
                     'simple weapons',
                 },
                 'tools': set(),
+                # use random.sample() here to choose two skills
+                'skills': {
+                    'History', 'Insight', 'Medicine', 'Persuasion', 'Religion',
+                },
             },
             'saving throws': {
                 'Wis', 'Cha',
             },
-            'class features': {
-                'Bardic Inspiration'
+            # use random.sample() here to choose one divine domain
+            'divine domain': {
+                'Knowledge': {
+                    'traits': 'Blessings of Knowledge',
+                    'domain spells': {
+                        'command', 'identify',
+                    },
+                }, 
+                'Life': {
+                    'traits': {
+                        'Disciple of Life',
+                    },
+                        # add to proficiency list
+                    'bonus proficiency': 'heavy armor',
+                    'domain spells': {
+                        'bless', 'cure wounds',
+                    },
+                }, 
+                'Light': {
+                    'traits': {
+                        # add extra cantrip to spells known
+                        'bonus cantrip',
+                        'Warding Flare'
+                    },
+                    'domain spells': {
+                        'burning hands',
+                        'faerie fire',
+                    },
+                }, 
+                'Nature': {
+                    # add to proficiency list
+                    'bonus proficiency': 'heavy armor'
+                    # gain one druid cantrip, use random to gain one of the following skill proficiencies
+                    'Acolyte of Nature': {
+                        'Animal Handling', 'Nature', 'Survival',
+                    },
+                    'domain spells': {
+                        'animal friendship', 'speak with animals',
+                    },
+                }, 
+                'Tempest': {
+                    'traits': {
+                        # add to proficiency list
+                        'bonus proficiency': {
+                            'heavy armor', 'martial weapons',
+                        },
+                        'Wrath of the Storm',
+                    },
+                    'domain spells': {
+                        'fog cloud', 'thunderwave',
+                    },
+                }, 
+                'Trickery': {
+                    'traits': 'Blessing of the Trickster',
+                    'domain spells': {
+                        'charm person', 'disguise self',
+                    },
+                }, 
+                'War': {
+                    'traits': {
+                        'War Priest',
+                    },
+                    'bonus proficiency': {
+                        # add to proficiency list
+                        'heavy armor', 'martial weapons',
+                    },
+                    'domain spells': {
+                        'divine favor', 'shield of faith',
+                    },
+                },
             },
             'spells and cantrips': {
-                'cantrips known': 2,
-                'spells known': 4,
+                'cantrips known': 3,
+                'spells known': 2,
             }
         },
     ]
